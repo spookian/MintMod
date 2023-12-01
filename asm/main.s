5968f719a09d920cb9f219cd91aee1ce7ddafb63
spookian <ianbow111@gmail.com>
2023-12-01 08:05:03 -0500

    okay typo fixed FOR REAL

--- asm/main.s
+++ asm/main.s
@@ -1,6 +1,6 @@
 .global __GOLEM_HOOK_START
 __GOLEM_HOOK_START:
-	lis %r3, (register_ReqWeapon+0x48000001-0x804d6660)@ha
+	lis %r3, (register_ReqWeapon+0x48000001-0x804d6660)@h
 	ori %r3, %r3, (register_ReqWeapon+0x48000001-0x804d6660)@l
 	lis %r4, 0x804D
 	stw %r3, 0x6660 (%r4)
