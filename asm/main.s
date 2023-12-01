6cd70bf9045aff8ab57a5bb17e2508c4452018b9
spookian <ianbow111@gmail.com>
2023-12-01 08:00:43 -0500

    silly typo

--- asm/main.s
+++ asm/main.s
@@ -2,7 +2,7 @@
 __GOLEM_HOOK_START:
 	lis %r3, (register_ReqWeapon+0x48000001-0x804d6660)@ha
 	ori %r3, %r3, (register_ReqWeapon+0x48000001-0x804d6660)@l
-	lis %r3, 0x804D
+	lis %r4, 0x804D
 	stw %r3, 0x6660 (%r4)
 
 	b vanilla_start
