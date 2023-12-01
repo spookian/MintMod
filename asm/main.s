c32f36a3e18e7e40dc8ece7e9ebee76213f72f6f
spookian <ianbow111@gmail.com>
2023-12-01 05:01:28 -0500

    final commit for the night

---
+++ asm/main.s
@@ -0,0 +1,8 @@
+.global __GOLEM_HOOK_START
+__GOLEM_HOOK_START:
+	lis %r3, (register_ReqWeapon+0x48000001-0x804d6660)@ha
+	ori %r3, (register_ReqWeapon+0x48000001-0x804d6660)@l
+	lis %r3, 0x804D
+	stw %r3, 0x6660 (%r4)
+
+	b vanilla_start
